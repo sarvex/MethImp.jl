f2d0875e3aec14d7a6d322ec83529d805b8ab5f1
Mike Innes <mike.j.innes@gmail.com>
2015-03-26 14:07:46 +0000

    fleshed out tags

--- src/MethImp.jl
+++ src/MethImp.jl
@@ -2,68 +2,27 @@ module MethImp
 
 using Lazy
 
-import Base: show
+# Objects are like objects in the OO sense –
+# a piece of data with some behaviour thrown in.
+
+# Methods are just names – messages that can
+# be sent to objects. Objects have implementations
+# for a given set of methods which they'll respond to.
+
+# Implementations are decoupled from methods. A single
+# method call could result in many implementations
+# responding, or none at all. Implementations themselves
+# declare what methods will trigger them.
+
+# Objects have "type tags" which are, well, tags.
+# An object can have more than one tag, and each
+# determines a set of implementations that form
+# the object's behaviour.
 
 include("weakset.jl")
 include("meta.jl")
-
-# Methods
-
-typealias Method Symbol
-
-# Implementations
-
-typealias Implementation Function
-
-name(f::Implementation) = f.env.name
-triggers(f::Implementation) = Set([name(f)])
-λ(f::Implementation) = f
-
-# Objects
-
-type Class
-#   var::Var
-  imps::Set{Implementation}
-  tags::Set{Symbol}
-  init::Function
-end
-
-typealias MethodSet Dict{Method, Set{Implementation}}
-
-type Object{T}
-  methods::MethodSet
-  tags::Set{Symbol}
-  data::T
-end
-
-# Construction & Manipulation
-
-typealias Objects WeakSet{Object}
-
-function collectmethods(imps)
-  methods = MethodSet()
-  for imp in imps
-    for method in triggers(imp)
-      push!(getset(methods, method, Set{Implementation}()), imp)
-    end
-  end
-  return methods
-end
-
-function Object(class::Class, args...)
-  Object(collectmethods(class.imps),
-         class.tags,
-         class.init(args...))
-end
-
-function call(obj::Object, method, args...)
-  imps = get(obj.methods, method, Set{Implementation}())
-  for imp in imps
-    imp(obj, args...)
-  end
-end
-
-tilde(object::Object, method::Symbol) = (args...) -> call(object, method, args...)
-tilde(object::Object, method::Symbol, args) = call(object, method, args...)
+include("tags.jl")
+include("implementations.jl")
+include("objects.jl")
 
 end # module
